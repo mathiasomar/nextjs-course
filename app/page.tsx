111eccdd24637aef01712045a0134242302bb21a
mathiasomar <ommykmathy@gmail.com>
2025-08-13 14:00:48 +0300

    configuring next config to allow download or display image fro external links

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="https://images.unsplash.com/photo-1754764987594-2236e7736115?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8"
           alt="Next.js logo"
           width={180}
           height={38}
