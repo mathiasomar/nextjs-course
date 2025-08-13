87ddf8413aaf778814257d182d509cb92c625690
mathiasomar <ommykmathy@gmail.com>
2025-08-13 13:47:02 +0300

    navigation to different routes

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        If you have any questions, feel free to reach out to us at
+        contact@ourcompany.com.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
